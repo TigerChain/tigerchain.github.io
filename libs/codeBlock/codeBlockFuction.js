fc8a8ca19dc02400a865e994e4abf38d11e112e3
TigerChain <git_dev@163.com>
2020-05-08 19:52:22 +0800

    Site updated: 2020-05-08 19:52:21

--- libs/codeBlock/codeBlockFuction.js
+++ libs/codeBlock/codeBlockFuction.js
@@ -1,5 +1,3 @@
-// 代码块功能依赖
-
-$(function () {
-    $('pre').wrap('<div class="code-area" style="position: relative"></div>');
-});
+// build time:Fri May 08 2020 19:46:21 GMT+0800 (GMT+08:00)
+$(function(){$("pre").wrap('<div class="code-area" style="position: relative"></div>')});
+//rebuild by neat 
