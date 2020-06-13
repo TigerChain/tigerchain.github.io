165cc8e3e39066b3aa7137aaa485a669133fec7d
TigerChain <git_dev@163.com>
2020-06-13 15:19:31 +0800

    Site updated: 2020-06-13 15:19:30

--- gitalk-config.js
+++ gitalk-config.js
@@ -1,4 +1,4 @@
-// build time:Tue May 26 2020 20:26:15 GMT+0800 (GMT+08:00)
+// build time:Fri Jun 05 2020 20:28:27 GMT+0800 (GMT+08:00)
 var gitalk=new Gitalk({clientID:"0ca9f84fc8ea0536e2d3",clientSecret:"502093771b7a1a37768c04ce6191ace747752dd7",repo:"jsblog",owner:"tigerchain",admin:["tigerchain"],id:window.location.pathname,distractionFreeMode:false});gitalk.render("gitalk-container");
 //rebuild by neat <script>
         document.querySelectorAll('.github-emoji')
